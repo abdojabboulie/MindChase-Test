17abb3a5fae3a8ef0a87e847076784ea3a807119
abdojabboulie <abdojabboulie@gmail.com>
2021-06-22 09:23:29 +0200

    kinda finished

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'MindChase Campus',
+  headline: 'Unlimited Fun Time with cool guys',
+  description:
+    'Safe Space to Learn and a Better tool to help you achive your Goals and Dreams ',
+  buttonLabel: 'Start your Journey',
+  imgStart: '',
+  img: 'images/svg-1.svg',
+  alt: 'Credit Card'
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: '100% Time Saving',
+  headline: 'Better three hours too soon than a minute too late. – William Shakespeare',
+  description:
+    'We have you covered with a lot of Valuble informations',
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: 'images/svg-5.svg',
+  alt: 'Vault'
+};
+
+export const homeObjThree = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'EASY SETUP',
+  headline: 'Super fast and simple onboarding process',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
+  imgStart: 'start',
+  img: 'images/svg-7.svg',
+  alt: 'Vault'
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'DATA ANALYTICS',
+  headline: 'Every transaction is stored on our secure cloud database',
+  description:
+    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: 'images/svg-8.svg',
+  alt: 'Vault'
+};
